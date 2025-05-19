2dbb54d0df53bce533e06e7046364572fee026d6
Rylie Castell <rylie.castell@gmail.com>
2025-05-19 23:31:05 +0200

    prepare project

--- SmartHome/SmartHomeApp.swift
+++ SmartHome/SmartHomeApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct SmartHomeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SmartHomeView()
         }
     }
 }
---
+++ SmartHome/Views/SmartHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  SmartHomeView.swift
+//  SmartHome
+//
+//  Created by Rylie Castell on 19.05.25.
+//
+
+import SwiftUI
+
+struct SmartHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmartHomeView()
+}
