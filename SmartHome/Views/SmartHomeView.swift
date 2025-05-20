ec7287d126397f1a50c878d3a261fb1496b7352e
Rylie Castell <rylie.castell@gmail.com>
2025-05-20 14:26:52 +0200

    add button functionality

--- SmartHome/Views/SmartHomeView.swift
+++ SmartHome/Views/SmartHomeView.swift
@@ -8,34 +8,25 @@
 import SwiftUI
 
 struct SmartHomeView: View {
-    @State var inputs: [String] = []
-    @State var textInput: String = ""
     @State var roomViewIsVisible: Bool = false
-
     @State var smartDevices: [SmartDevice] = [
         SmartDevice(name: "Wohnzimmerlicht", type: .light()),
         SmartDevice(name: "Heizung", type: .thermostat()),
         SmartDevice(name: "Haustür", type: .lock()),
     ]
+    @State var newLight: SmartDevice = SmartDevice(name: "", type: .light())
 
     var body: some View {
         HStack {
-            TextField("Gib einen Namen ein...", text: $textInput)
+            TextField("Neues Licht", text: $newLight.name)
             Button("Hinzufügen") {
-                if textInput.count >= 1 {
-                    inputs.append(textInput)
-                    textInput = ""
+                if newLight.name.count >= 1 {
+                    smartDevices.append(newLight)
+                    newLight = SmartDevice(name: "", type: .light())
                 }
             }
         }
         .padding()
-        ScrollView {
-            VStack {
-                ForEach(inputs, id: \.self) { input in
-                    Text(input)
-                }
-            }
-        }
         SmartDeviceView(smartDevices: $smartDevices)
         VStack {
             RoomView(roomViewIsVisible: $roomViewIsVisible)
