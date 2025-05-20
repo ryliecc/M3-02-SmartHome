305565fd6f2334c4f7a663fe6a173dbc34da4fe1
Rylie Castell <rylie.castell@gmail.com>
2025-05-21 01:34:39 +0200

    import fonts

---
+++ SmartHome/Utilities/Fonts.swift
@@ -0,0 +1,17 @@
+//
+//  Fonts.swift
+//  SmartHome
+//
+//  Created by Rylie Castell on 21.05.25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Fonts {
+    static let headerTitle = Font.custom("Aboreto-Regular", size: 48)
+    static let deviceName = Font.custom("Raleway-Regular", size: 20)
+    static let deviceTypeName = Font.custom("Raleway-Light", size: 14)
+    static let roomViewLabel = Font.custom("Raleway-Medium", size: 16)
+    static let temperatureLabel = Font.custom("Raleway-Medium", size: 24)
+}
