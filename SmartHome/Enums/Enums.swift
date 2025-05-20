d52f801ce962d3a2333daee94d3892dc915fd874
Rylie Castell <rylie.castell@gmail.com>
2025-05-20 11:29:04 +0200

    add device type enum

---
+++ SmartHome/Enums/Enums.swift
@@ -0,0 +1,14 @@
+//
+//  Enums.swift
+//  SmartHome
+//
+//  Created by Rylie Castell on 20.05.25.
+//
+
+import Foundation
+
+enum DeviceType {
+    case light
+    case thermostat
+    case lock
+}
