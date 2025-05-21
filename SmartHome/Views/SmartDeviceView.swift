ffac3786b04d78b361f1ee315dc2deb475cf49de
Rylie Castell <rylie.castell@gmail.com>
2025-05-22 00:56:04 +0200

    add styling and functionality to smart device list

--- SmartHome/Views/SmartDeviceView.swift
+++ SmartHome/Views/SmartDeviceView.swift
@@ -9,20 +9,104 @@ import SwiftUI
 
 struct SmartDeviceView: View {
     @Binding var smartDevices: [SmartDevice]
-    
+
     var body: some View {
         ScrollView {
-            VStack {
+            VStack(spacing: 16) {
                 ForEach($smartDevices, id: \.id) { smartDevice in
-                    HStack {
-                        Image(systemName: "lightbulb")
-                        Text(smartDevice.wrappedValue.name)
-                        Spacer()
-                        Text(smartDevice.wrappedValue.type.rawValue)
+                    if smartDevice.wrappedValue.type == .light {
+                        HStack {
+                            Image(systemName: "lightbulb")
+                                .font(.system(size: 32))
+                            Spacer()
+                            VStack {
+                                Text(smartDevice.wrappedValue.name)
+                                    .font(Fonts.deviceName)
+                                Spacer()
+                                Text(smartDevice.wrappedValue.type.rawValue)
+                                    .font(Fonts.deviceTypeName)
+                            }
+                            .padding()
+                            Spacer()
+                            Toggle("", isOn: smartDevice.isOn)
+                                .labelsHidden()
+                        }
+                        .frame(height: 65)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 9).fill(
+                                Color(
+                                    red: 217 / 255,
+                                    green: 217 / 255,
+                                    blue: 217 / 255
+                                )
+                            )
+                        )
+                    } else if smartDevice.wrappedValue.type == .thermostat {
+                        HStack {
+                            Image(systemName: "thermometer.high")   .font(.system(size: 32))
+                            Spacer()
+                            VStack {
+                                Text(smartDevice.wrappedValue.name)
+                                    .font(Fonts.deviceName)
+                                Spacer()
+                                Text(smartDevice.wrappedValue.type.rawValue)
+                                    .font(Fonts.deviceTypeName)
+                            }
+                            .padding()
+                            Spacer()
+                            VStack {
+                                Text("\(String(format: "%.0f", smartDevice.wrappedValue.temperature))°C")
+                                    .font(Fonts.temperatureLabel)
+                                Stepper("", value: smartDevice.temperature, step: 1)
+                                    .labelsHidden()
+                            }
+                        }
+                        .frame(height: 65)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 9).fill(
+                                Color(
+                                    red: 217 / 255,
+                                    green: 217 / 255,
+                                    blue: 217 / 255
+                                )
+                            )
+                        )
+                    } else {
+                        HStack {
+                            Image(systemName: "lock.fill")   .font(.system(size: 32))
+                            Spacer()
+                            VStack {
+                                Text(smartDevice.wrappedValue.name)
+                                    .font(Fonts.deviceName)
+                                Spacer()
+                                Text(smartDevice.wrappedValue.type.rawValue)
+                                    .font(Fonts.deviceTypeName)
+                            }
+                            .padding()
+                            Spacer()
+                            Toggle("", isOn: smartDevice.isLocked)
+                                .labelsHidden()
+                        }
+                        .frame(height: 65)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 9).fill(
+                                Color(
+                                    red: 217 / 255,
+                                    green: 217 / 255,
+                                    blue: 217 / 255
+                                )
+                            )
+                        )
                     }
-                    .padding()
                 }
             }
+            .padding()
         }
     }
 }
